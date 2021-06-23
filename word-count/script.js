b34812d859143d70ee43adf6c138c95a1f763a8e
lrnzaa <56107858+lrnzaa@users.noreply.github.com>
2021-06-23 10:50:46 +0700

    add clear button

--- word-count/script.js
+++ word-count/script.js
@@ -1,9 +1,10 @@
 let button = document.getElementById('btnCount');
+let clear = document.getElementById('btnClear');
 
 button.addEventListener('click', function() {
-    let word = document.getElementById('txtInput').value;
+    let word = document.getElementById("txtInput").value;
     let count = 0;
-    let output = document.getElementById('op');
+    let output = document.getElementById("op");
 
     if(word.length <= 0)
     {
@@ -23,4 +24,11 @@ button.addEventListener('click', function() {
     }
 
     output.innerHTML = `${count}  words.`
+});
+
+
+clear.addEventListener('click', function()
+{
+    document.getElementById("txtInput").value = "";
+    document.getElementById("op").innerHTML = "";
 });
